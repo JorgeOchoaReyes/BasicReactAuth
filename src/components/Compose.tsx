fd754afae3081ceaf5d99d0563db94327beca286
Jorge Ochoa Reyes <jorgeochoareyes@gmail.com>
2022-06-13 21:56:58 -0700

    Better method for hooks

--- src/components/Compose.tsx
+++ src/components/Compose.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
+import { json } from 'stream/consumers';
+import { useCompose } from '../hooks/useCompose';
 import { Title } from './UI';
 
 interface ComposeProps {
@@ -8,13 +11,14 @@ interface ComposeProps {
 //Compose
 //  ○ Includes recipient, title, and body
 
-
-
-
 export const Compose: React.FC<ComposeProps> = ({}) => {
+    const {apiData, isLoading, serverError, fetchData } = useCompose("https://swapi.dev/api/people/1/", "GET");
+    console.log(apiData, isLoading, serverError)
     return (
     <>
         <Title> Compose </Title> 
+        {apiData && !isLoading ? JSON.stringify(apiData) : null}
+        <Button onClick={() => fetchData()} />
     </>
     );
 }
